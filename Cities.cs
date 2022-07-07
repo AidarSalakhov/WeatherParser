08d6da0315a1558d6467415753c6d5f5613ee359
Aidar Salakhov <adr-sal@yandex.ru>
2022-07-07 11:59:44 +0300

    Develop

--- Cities.cs
+++ Cities.cs
@@ -11,5 +11,19 @@ namespace WeatherParser
         private string _region { get; set; }
         private string _regionIdentificationLetter { get; set; }
         private string _city { get; set; }
+        private string _weather { get; set; }
+
+        public Cities(string region, string regionIdentificationLetter, string city, string weather)
+        {
+            _region = region;
+            _regionIdentificationLetter = regionIdentificationLetter;
+            _city = city;
+            _weather = weather;
+        }
+
+        public static void GetWeather()
+        {
+
+        }
     }
 }
