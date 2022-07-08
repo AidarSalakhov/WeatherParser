149c98e8233c79892f6ceac7ccc91187b0342103
Aidar Salakhov <adr-sal@yandex.ru>
2022-07-08 10:47:00 +0300

    Develop

--- Regions.cs
+++ Regions.cs
@@ -10,39 +10,27 @@ namespace WeatherParser
     internal class Regions
     {
         private string _regionName { get; set; }
-        private string _regionNameFirstLetter { get; set; }
         private string _regionUrl { get; set; }
 
         public static List<Regions> _listOfRegions = new List<Regions>();
-                              
+
         public static List<Regions> ParseRegions(string url)
         {
             HtmlWeb htmlWeb = new HtmlWeb();
 
             var htmlDoc = htmlWeb.Load(url);
 
-            var node = htmlDoc.DocumentNode.SelectNodes("//ul[@class='cities reg']");
+            var citiesReg = htmlDoc.DocumentNode.SelectNodes("//ul[@class='cities reg']//li [not(contains(@class,'cities-letter'))]");
 
-            int regionsCount = node.Count();
-
-            foreach (var item in node)
+            foreach (var item in citiesReg)
             {
                 Regions region = new Regions();
 
-                region._regionNameFirstLetter = item.SelectSingleNode(".//li[@class='cities-letter']").InnerText;
-
-                    Console.WriteLine(region._regionNameFirstLetter);      
-
-                region._regionName = item.SelectSingleNode(".//li").InnerText;
+                region._regionName = item.InnerText;
 
                 Console.WriteLine(region._regionName);
-
-                //region._regionUrl = item.SelectSingleNode(".//li//a[@href='resumeListItem']").InnerText;
-
-                _listOfRegions.Add(region);
+                                
             }
-
-
             return _listOfRegions;
         }
     }
