0e09f9cba10dc977abc2393afd488fa5a61fac4f
Aidar Salakhov <adr-sal@yandex.ru>
2022-07-11 13:57:18 +0300

    Добавил кривой парсинг текущей погоды

--- Weather.cs
+++ Weather.cs
@@ -3,24 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace WeatherParser
 {
     internal class Weather
     {
-        private string _temperatureNow;
-        private string _temperatureFelt;
-        private string _pressure;
-        private string _humidity;
-        private string _wind;
-        private string _windGust;
-        private string _cloudiness;
-        private string _visibility;
-        private string _uvIndex;
-
-        public static void GetWeatherNow(string url)
+        public struct WeatherStruct
         {
+            internal string parameter;
+            internal string value;
+        }
+
+        public static List<WeatherStruct> listOfWeather = new List<WeatherStruct>();
+
+        public static List<WeatherStruct> GetWeatherNow(string url)
+        {
+            HtmlWeb htmlWeb = new HtmlWeb();
+
+            var htmlDoc = htmlWeb.Load(url);
+
+            //var weatherNow = htmlDoc.DocumentNode.SelectNodes("//dt//span[not(contains(@class, 'min-max-tempCold')) and not(contains(@class, 'min-max-tempHot'))]");
             
+            var weatherNow = htmlDoc.DocumentNode.SelectNodes("//div[@id='weather-now-description']//dl");
+
+            foreach (var item in weatherNow)
+            {
+                WeatherStruct weatherStruct = new WeatherStruct();
+
+                weatherStruct.parameter = item.InnerText;
+
+                Console.WriteLine(weatherStruct.parameter);
+
+                weatherStruct.value = item.GetAttributeValue("span", null);
+
+                Console.WriteLine(weatherStruct.value);
+
+                listOfWeather.Add(weatherStruct);
+            }
+
+            return listOfWeather;
         }
 
     }
