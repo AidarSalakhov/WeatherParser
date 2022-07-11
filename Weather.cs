7a5bafdc34a9cfb3f460373c765f00f6dbd21c93
Aidar Salakhov <adr-sal@yandex.ru>
2022-07-11 16:19:26 +0300

    Develop

--- Weather.cs
+++ Weather.cs
@@ -23,10 +23,44 @@ namespace WeatherParser
 
             var htmlDoc = htmlWeb.Load(url);
 
+            var weatherNowNodes = htmlDoc.DocumentNode.SelectNodes("//div[@class='weather-now-info']");
+
+            WeatherStruct weatherStruct = new WeatherStruct();
+
+            weatherStruct.parameter = $"{htmlDoc.DocumentNode.SelectSingleNode("//h1").InnerText}. Сейчас";
+
+            Console.WriteLine(weatherStruct.parameter);
+
+            weatherStruct.value = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='weather-now-info']//b[contains(text(), ':')]").InnerText;
+
+            Console.WriteLine(weatherStruct.value);
+
+            //h1 - "Погода в Казани"
+
+            //div[@class='weather-now-info']//span[not(contains(text(), '°'))] - "Сейчас"
+
+            //div[@class='weather-now-info']//b - "14:45, 11 июл"
+
+            //div[@class='weather-now-info']//span/@title - "Преимущественно ясно"
+
+            //div[@id='weather-now-number'] - "+30" 
+
+            //div[@id='weather-now-number']//span - "°"
+
+
+            return listOfWeather;
+
+        }
+
+        public static List<WeatherStruct> GetWeatherNowAdditionalInformation(string url)
+        {
+            HtmlWeb htmlWeb = new HtmlWeb();
+
+            var htmlDoc = htmlWeb.Load(url);
+
             var parametersNodes = htmlDoc.DocumentNode.SelectNodes("//dt//span[not(contains(@class, 'min-max-tempCold')) and not(contains(@class, 'min-max-tempHot'))]");
 
             var valueNodes = htmlDoc.DocumentNode.SelectNodes("//div[@id='weather-now-description']//dl//dd");
-            
 
             for (int i = 0; i < parametersNodes.Count; i++)
             {
